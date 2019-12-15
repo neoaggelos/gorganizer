6d7afb879070e8233ef6e830139e8d5941a5a320
Aggelos Kolaitis <neoaggelos@gmail.com>
2019-12-15 11:36:40 +0200

    Add `.exe` extension on windows builds

--- build.sh
+++ build.sh
@@ -6,7 +6,8 @@ mkdir -p build
 set -x
 for arch in 386 amd64 arm; do
   for os in darwin windows linux; do
-    GOARCH=$arch GOOS=$os go build -o build/gorganizer-$version-$os-$arch gorganizer.go
+    [[ "x$os" = "xwindows" ]] && ext=".exe" || ext=""
+    GOARCH=$arch GOOS=$os go build -o "build/gorganizer-${version}-${os}-${arch}${ext}" gorganizer.go
   done
 done
 set +x
